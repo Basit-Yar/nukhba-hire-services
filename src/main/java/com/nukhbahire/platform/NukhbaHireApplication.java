2a0cab46d2820e406bd0f09cf75c4ecc1351de65
SyedBasit <basityar6@gmail.com>
2025-10-18 20:03:47 +0530

    project setup

---
+++ src/main/java/com/nukhbahire/platform/NukhbaHireApplication.java
@@ -0,0 +1,13 @@
+package com.nukhbahire.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NukhbaHireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NukhbaHireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nukhbahire/platform/NukhbaHireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nukhbahire.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NukhbaHireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
