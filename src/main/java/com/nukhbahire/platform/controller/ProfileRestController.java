028be0c80182e1227d1dd26d4ffbff3852d6b377
SyedBasit <basityar6@gmail.com>
2025-10-27 23:49:25 +0530

    adding initial structure for Profile module with controller, service, dao, and repository

---
+++ src/main/java/com/nukhbahire/platform/controller/ProfileRestController.java
@@ -0,0 +1,8 @@
+package com.nukhbahire.platform.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProfileRestController {
+
+}
---
+++ src/main/java/com/nukhbahire/platform/service/ProfileService.java
@@ -0,0 +1,8 @@
+package com.nukhbahire.platform.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfileService {
+
+}
