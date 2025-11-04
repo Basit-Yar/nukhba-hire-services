cb08334f249b1b84e3f32da92b3fb5acac805891
SyedBasit <basityar6@gmail.com>
2025-11-04 20:19:14 +0530

    adding logger.

--- src/main/java/com/nukhbahire/platform/exception/GlobalExceptionHandler.java
+++ src/main/java/com/nukhbahire/platform/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.nukhbahire.platform.exception;
 
-import com.fasterxml.jackson.core.JsonParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -12,8 +13,13 @@ import javax.servlet.http.HttpServletRequest;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger _log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponseDTO> handleBusinessException(BusinessException exception, HttpServletRequest request) {
+
+        _log.error("Unhandled exception: {}", exception.getMessage(), exception);
+
         ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(
                 HttpStatus.BAD_REQUEST.value(),
                 exception.getErrorCode(),
@@ -26,6 +32,9 @@ public class GlobalExceptionHandler {
     // Handle JSON parse / malformed body errors
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorResponseDTO> handleJsonParseException(HttpMessageNotReadableException ex, HttpServletRequest request) {
+
+        _log.error("Unhandled exception: {}", ex.getMessage(), ex);
+
         ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(
                 HttpStatus.BAD_REQUEST.value(),
                 "JSON_PARSE_ERROR",
@@ -38,6 +47,9 @@ public class GlobalExceptionHandler {
     // Fallback: handle everything else
     @ExceptionHandler(Throwable.class)
     public ResponseEntity<ErrorResponseDTO> handleAll(Throwable ex, HttpServletRequest request) {
+
+        _log.error("Unhandled exception: {}", ex.getMessage(), ex);
+
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
         ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(
                 status.value(),
